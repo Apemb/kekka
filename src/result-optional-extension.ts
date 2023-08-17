967aacc3cfe87c797a3a9d6fb8e1cb6a72a9d805
Antoine Boileau <antoine@boileau.io>
2023-08-17 15:57:05 +0200

    Add result.toOptional function

--- src/result-optional-extension.ts
+++ src/result-optional-extension.ts
@@ -1,5 +1,5 @@
 import {Failure, Result, Success} from './result'
-import {Optional} from './optional'
+import {Empty, Optional} from './optional'
 
 declare module './optional' {
   interface Optional<Value> {
@@ -11,4 +11,20 @@ Optional.prototype.toResult = function <Value>(failure: Error): Result<Value> {
   return this
     .map((value) => Success(value))
     .orElse(Failure(failure))
+}
+
+declare module './result' {
+  interface Result<Value> {
+    toOptional(): Optional<Value>
+  }
+}
+
+Result.prototype.toOptional = function <Value>(): Optional<Value> {
+  if (this.isFailure()) {
+    return Empty<Value>()
+  }
+
+  const associatedValue = this.unwrap()
+
+  return Optional.ofNullable(associatedValue)
 }
--- test/result-optional-extension.test.ts
+++ test/result-optional-extension.test.ts
@@ -1,7 +1,7 @@
 import {expect} from './test-helper'
 
 import('../src/result-optional-extension')
-import {Empty, Some} from '../src/optional'
+import {Empty, isOptional, Some} from '../src/optional'
 import {Success, Failure} from '../src/result'
 
 describe('Optional Extension', () => {
@@ -22,6 +22,41 @@ describe('Optional Extension', () => {
       const error = new Error('some error')
       const result = Failure(error)
       expect(empty.toResult(error)).to.deep.equal(result)
+    })
+  })
+})
+
+describe('Result Extension', () => {
+
+  describe('toOptional', () => {
+    it('should return Some with value if Result is Success with not undef or null value', () => {
+      const value = 'some value'
+      const result = Success(value)
+
+      const optional = result.toOptional()
+
+      expect(isOptional(optional)).to.be.true
+      expect(optional.orElse('another value')).to.equal(value)
+    })
+
+    it('should return Empty if Result is a Failure', () => {
+      const error = new Error('some error')
+      const result = Failure(error)
+
+      const optional = result.toOptional()
+
+      expect(isOptional(optional)).to.be.true
+      expect(optional.isEmpty()).to.be.true
+    })
+
+    it('should return Empty with value if Result is Success with undef or null value', () => {
+      const value = null
+      const result = Success(value)
+
+      const optional = result.toOptional()
+
+      expect(isOptional(optional)).to.be.true
+      expect(optional.isEmpty()).to.be.true
     })
   })
 })
